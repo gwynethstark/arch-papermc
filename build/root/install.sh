6980fa7aa21292cc956793b251ad6a55124122e8
Gwen van Vlokhoven <gwenvvlokhoven@gmail.com>
2021-12-15 17:46:37 +0100

    no need for release tag since it always uses newest

--- build/root/install.sh
+++ build/root/install.sh
@@ -3,14 +3,6 @@
 # exit script if return code != 0
 set -e
 
-# release tag name from build arg, stripped of build ver using string manipulation
-release_tag_name="${1//-[0-9][0-9]/}"
-
-if [[ -z "${release_tag_name}" ]]; then
-	echo "[warn] Release tag name from build arg is empty, exiting script..."
-	exit 1
-fi
-
 # build scripts
 ####
 
